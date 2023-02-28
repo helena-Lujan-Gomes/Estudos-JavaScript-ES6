a2da695d5394aef751414686e6141b01e8fe5c5f
Helena Gomes <lujanhelena@gmail.com>
2023-02-28 19:59:50 -0300

    tudo é objeto

---
+++ tudo-e-objeto.js
@@ -0,0 +1,22 @@
+var nome = 'Helena';
+var nomeMinusculo = nome.toLowerCase();
+
+var btn = document.querySelector('.btn');
+
+//EXERCÍCIO
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'Helena';
+//fixed
+//length
+//slice
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn');
+//addEventListener
+//appendChild
+//id
+//innerHtml
+//outerHtml
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
