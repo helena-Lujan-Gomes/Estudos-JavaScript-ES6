c76cb16a4bd96e65fbdefa84c5fc331278be2937
Helena Gomes <lujanhelena@gmail.com>
2023-03-07 14:37:40 -0300

    comandos de entrada

---
+++ comandos-de-entrada/script.js
@@ -0,0 +1,7 @@
+//alert('teste');
+//script é lido de cima para baixo
+var nome = prompt("Qual seu nome?");
+var sobrenome = prompt("Qual seu sobrenome?");
+console.log("Olá " + nome + " " + sobrenome);
+
+document.write("<h1> Bem vindo "+ nome + " </h1>")
