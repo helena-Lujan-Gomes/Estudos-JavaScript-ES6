31db3d73aa09bed7f0b5de2095cd5073851fc809
Helena Gomes <lujanhelena@gmail.com>
2023-03-07 15:03:11 -0300

    entendendo funções

---
+++ entendendo-funcoes/script.js
@@ -0,0 +1,23 @@
+var area = document.getElementById('area')
+
+function entrar() {
+  var nome = prompt("Digite seu nome");
+  area.innerHTML = "Bem vindo " + nome;
+
+  if(nome === '' || nome === null) {
+    alert("Ops algo deu errado")
+    area.innerHTML = "Clique no botao para acessar"
+  } else {
+    area.innerHTML = "Bem vindo " + nome + " ";
+
+    let botaoSair = document.createElement("button");
+    botaoSair.innerHTML = "Sair da conta";
+    botaoSair.onclick = sair;
+    area.appendChild(botaoSair);
+  }
+}
+
+function sair() {
+  alert("Até mais!");
+  area.innerHTML = "Você saiu!"
+}
