b2131a39418b4441009fc1355f6649c1762a34e9
Helena Gomes <lujanhelena@gmail.com>
2023-03-07 18:05:43 -0300

    aprendendo loops

---
+++ aprendendo loops/script.js
@@ -0,0 +1,12 @@
+var x = 5;
+while(x < 10) {
+  document.write("<br> O valor do X é: + x");
+  x++;
+}
+
+
+var valor = 30
+for(a = 0; a <= valor; a++) {
+  document.write("<br> Valor do A é: " + a);
+  console.log(a + 10);
+}
