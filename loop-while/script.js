e8a631f64610b71b46e7e3af648e5c7350e8d4d8
Helena Gomes <lujanhelena@gmail.com>
2023-03-14 17:11:13 -0300

    loop while

---
+++ loop-while/script.js
@@ -0,0 +1,17 @@
+let numero = 0;
+while (numero <= 10) {
+  console.log(`O número da vez é ${numero}`);
+  numero++;
+}
+
+for(let numero = 0; numero <= 10; numero++) {
+  console.log(`O número da vez é ${numero}`);
+}
+
+/*while (true) {
+
+} */ //loop infinito
+
+while(condicional) {
+  ///
+}
