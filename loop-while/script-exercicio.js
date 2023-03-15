e46903d3cd86e93de1d192e93548e72d74b70678
Helena Gomes <lujanhelena@gmail.com>
2023-03-15 10:15:50 -0300

    loop while

---
+++ loop-while/script-exercicio.js
@@ -0,0 +1,12 @@
+//1. Faça um lop que mostre todas as frutas
+let frutas = ['Maça', 'Uva', 'Banana'];
+for (let x in fruits) {
+  console.log(fruits[x]);
+}
+
+//2. Conte de 1 até 100 através de um WHILE
+let n = 1;
+while (n <= 100) {
+  console.log(n);
+  n++;
+}
