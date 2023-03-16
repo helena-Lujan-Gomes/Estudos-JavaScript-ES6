cc2e4cce6fcd8283a724a4d81a442b204ef9d360
Helena Gomes <lujanhelena@gmail.com>
2023-03-16 20:14:51 -0300

    eventos de teclado

---
+++ dom/eventos-de-teclado/script.js
@@ -0,0 +1,16 @@
+function apertou() {
+  console.log("Apertou!");
+
+}
+
+function segurou() {
+  console.log("Segurou!");
+
+}
+
+function soltou() {
+  console.log("Soltou!");
+
+}
+
+document.addEventListener('keyup', soltou);
