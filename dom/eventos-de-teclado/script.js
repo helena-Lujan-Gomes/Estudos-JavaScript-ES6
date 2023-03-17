2a0597f0c0bb6d5e623ee66ab3f681d15ff88867
Helena Gomes <lujanhelena@gmail.com>
2023-03-17 09:23:57 -0300

    eventos de teclado

--- dom/eventos-de-teclado/script.js
+++ dom/eventos-de-teclado/script.js
@@ -14,3 +14,21 @@ function soltou() {
 }
 
 document.addEventListener('keyup', soltou);
+
+function soltou(event) {
+  console.log(event.code); //mais específico ex: shiftleft
+  console.log(event.key); //mais direto ex:shift
+  console.log(event.shiftKey); //boolean
+}
+
+//funcion soltou(event) {
+ // console.log('tecla apertada: ' + event.code);
+ //console.log('shift? ' + event.shiftKey);
+ //console.log('alt? ' + event.altKey);
+ //console.log('--');
+
+//}
+
+const input = document.querySelector('input');
+input.addEventListener('keyup', soltou);
+
