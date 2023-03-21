15bc2b7d66911f1e7ba2080eeb976c41f7a7271e
Helena Gomes <lujanhelena@gmail.com>
2023-03-21 17:26:29 -0300

    o que é um callback

---
+++ requisicoes/callback/script.js
@@ -0,0 +1,18 @@
+/*document.querySelector('#botao')
+.addEventListener('click', () => {
+ alert("Clicou no botao");
+}) */
+
+function clickCallback() {
+  alert("Clicou no botao");
+  };
+  document.querySelector('#botao')
+  .addEventListener('click', clickCallback );
+
+/*
+função para quando clicar no botão:
+-requisção a url X
+-tá aqui o callback para quando você tiver a resposta:
+-- callbackMaroto
+*/
+
