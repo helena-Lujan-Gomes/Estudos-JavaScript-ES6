82d4e752edd36c4ae20facb06a64c0005f34907c
Helena Gomes <lujanhelena@gmail.com>
2023-03-22 16:20:01 -0300

    métodos de envio de requisição

--- requisicoes/callback/promise/script.js
+++ requisicoes/callback/promise/script.js
@@ -2,6 +2,7 @@
 //Promise
 
 function clicou() {
+  //GET, POST, PUT, DELETE
   fetch("https://jsonplaceholder.typicode.com/posts")
 
    .then((response) => {
@@ -20,4 +21,27 @@ function clicou() {
 
     })
 }
+
+function inserir() {
+  fetch("https://jsonplaceholder.typicode.com/posts",
+  {
+    method: 'POST',
+    headers: {
+      'Content-Tupe': 'application/json'
+    },
+    body: JSON.stringify({
+      title: 'Titulo de teste',
+      body: 'Corpo de teste',
+      userId: 2
+    })
+  })
+  .then((response) => {
+    return response.json();
+  })
+  .then((json) => {
+    console.log(json);
+  });
+}
+
 document.querySelector("#botao").addEventListener("click", clicou);
+document.querySelector("#inserir").addEventListener("click", clicou);
