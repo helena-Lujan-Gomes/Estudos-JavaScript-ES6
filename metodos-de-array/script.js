36885dae9e08d7388bda98d2062b08d614bb0bf6
Helena Gomes <lujanhelena@gmail.com>
2023-03-27 16:36:02 -0300

    métodos de arrays

--- metodos-de-array/script.js
+++ metodos-de-array/script.js
@@ -31,3 +31,22 @@ lista2 = lista.every(function(item) {
 
   return (item > 3) ? true : false;
 })
+
+lista2 = lista.find(function(item) {
+  return (item == 16) ? true : false;
+});
+
+lista2 = lista.findIndex(function(item) {
+  return (item == 16) ? true : false;
+});
+
+let lista3 = [
+  {id:1, nome: "Helena", sobrenome: "Gomes"},
+  {id:2, nome: "Diego", sobrenome: "Aguilar"},
+  {id:3, nome: "Fabio", sobrenome: "Veraldi"}
+];
+
+let pessoa = lista3.find(function(item){
+  return (item.id == 3) ? true : false;
+})
+
