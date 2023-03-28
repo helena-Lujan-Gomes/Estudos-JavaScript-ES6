edcaa24a222d8712fba41541cd66507793f4207a
Helena Gomes <lujanhelena@gmail.com>
2023-03-28 11:24:35 -0300

    Matemática

---
+++ matematica/script.js
@@ -0,0 +1,11 @@
+let novoValor = Math.PI;
+let novoValor = Math.round(3.67);
+let novoValor = Math.floor(3.53);
+let novoValor = Math.ceil(3.99);
+let novoValor = Math.abs( -9.68686 );
+let novoValor = Math.min(7, 100, 600);
+let novoValor = Math.max(7, 100, 600);
+let novoValor = Math.floor(Math.random() * 100);
+Math.sin
+Math.cos
+Math.tan
