4f144b729b09c56a8036887d3960532e5e400c4c
Helena Gomes <lujanhelena@gmail.com>
2023-03-28 12:46:18 -0300

    template string

---
+++ template-string/script.js
@@ -0,0 +1,5 @@
+let nome = 'Helena';
+let idade = 26;
+
+//let frase = "Meu nome é '+nome+', eu tenho '+idade+' anos e ano que vem eu farei '+(idade+1)+' anos";
+let frase = `Meu nome é ${nome}, eu tenho ${idade} anos e ano que vem eu farei ${idade+1} anos `;
